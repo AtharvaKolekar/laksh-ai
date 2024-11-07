7df1705d34467802ed88b6aff3e0145ac2c49833
Atharva Kolekar <118675133+AtharvaKolekar@users.noreply.github.com>
2024-11-08 00:35:57 +0530

    Created Dashboard

--- src/app/(protected)/Dashboard/page.jsx
+++ src/app/(protected)/Dashboard/page.jsx
@@ -8,62 +8,206 @@ import { initFirebase } from "@/lib/firebase";
 
 import { getDatabase, ref, set, get, child } from "firebase/database";
 import { useRouter } from "next/navigation";
-import { Button, Modal } from "antd";
+import { Button, Modal, Empty, Typography, Steps } from "antd";
 import { WechatOutlined } from "@ant-design/icons";
 import Search from "antd/es/input/Search";
 
+const progresses = [
+    {
+        type: "Roadmap",
+        title: "Web Development",
+        subTitle: "Step",
+        completedStep: 2,
+        totalSteps: 5,
+    },
+    {
+        type: "Virtual Internship",
+        title: "Backend Development Internship",
+        subTitle: "Task",
+        completedStep: 3,
+        totalSteps: 7,
+    },
+    {
+        type: "Roadmap",
+        title: "Data Science Path",
+        subTitle: "Step",
+        completedStep: 0,
+        totalSteps: 6,
+    },
+    {
+        type: "Virtual Internship",
+        title: "Machine Learning Internship",
+        subTitle: "Task",
+        completedStep: 1,
+        totalSteps: 4,
+    },
+];
+
 export default function DashboardLayout({ children }) {
-  const router = useRouter();
-  const [open, setOpen] = useState(false);
-  const [loading, setLoading] = useState(true);
+    const router = useRouter();
+    const [open, setOpen] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const [isStarted, setIsStarted] = useState(false);
 
-  const app = initFirebase();
-  const database = getDatabase(app);
-  const user = useUser();
+    const app = initFirebase();
+    const database = getDatabase(app);
+    const user = useUser();
 
-  const [name, setName] = useState("");
-  const showLoading = () => {
-    setOpen(true);
-    setLoading(true);
+    const [name, setName] = useState("");
+    const showLoading = () => {
+        setOpen(true);
+        setLoading(true);
 
-    setLoading(false);
-  };
-  return (
-    <main>
-      {children}
-      {/* <Header title={"Welcome " + name + "!"} profile logout={signOut} /> */}
-      <div className={styles.container}>
-        <Button
-          shape="circle"
-          className={styles.btn}
-          variant="outlined"
-          icon={<WechatOutlined />}
-          onClick={showLoading}
-          size={"large"}
-        />
-        <Modal
-          width={400}
-          title={<p>LakshAI</p>}
-          style={{
-            position: "fixed",
+        setLoading(false);
+    };
+    useEffect(() => {
+        setIsStarted(progresses.length > 0);
+    }, [progresses]);
 
-            top: 10,
-            right: 20,
-          }}
-          loading={loading}
-          footer={null}
-          open={open}
-          onCancel={() => setOpen(false)}
-        >
-          <Search
-            placeholder="Ask me anything"
-            allowClear
-            enterButton="Send"
-            size="large"
-            onSearch={() => {}}
-          />
-        </Modal>
-      </div>
-    </main>
-  );
+    return (
+        <main>
+            {children}
+            {/* <Header title={"Welcome " + name + "!"} profile logout={signOut} /> */}
+            <div className={styles.container}>
+                <h4 className={styles.conTitle}>Quick Access</h4>
+                <div className={styles.modules}>
+                    <div
+                        className={styles.module}
+                        onClick={() =>
+                            router.push("/Dashboard/RecommendCareer")
+                        }
+                    >
+                        <img src="/career.png" alt="career" />
+                        <div className={styles.module_title}>
+                            Career Recommendation
+                        </div>
+                    </div>
+                    <div
+                        className={styles.module}
+                        onClick={() => router.push("/Dashboard/Roadmaps")}
+                    >
+                        <img src="/roadmap.png" alt="roadmap" />
+                        <div className={styles.module_title}>
+                            Roadmap Builder
+                        </div>
+                    </div>
+                    <div
+                        className={styles.module}
+                        onClick={() => router.push("/Dashboard/Internship")}
+                    >
+                        <img src="/internship.png" alt="internship" />
+                        <div className={styles.module_title}>
+                            Virtual Internship
+                        </div>
+                    </div>
+                    <div
+                        className={styles.module}
+                        onClick={() => router.push("/Dashboard/Mentors")}
+                    >
+                        <img src="/mentor.png" alt="mentor" />
+                        <div className={styles.module_title}>
+                            Mentor Guidance{" "}
+                        </div>
+                    </div>
+                </div>
+                <h4 className={styles.conTitle}>Top Career Opportunities</h4>
+                <div className={styles.topCareers}>
+                    <div className={styles.careers}>
+                        <div className={styles.career + " " + styles.a}>
+                            <p>1. Data Scientist</p>
+                        </div>
+                        <div className={styles.career + " " + styles.b}>
+                            <p>2. Cloud Architect</p>
+                        </div>
+                        <div className={styles.career + " " + styles.c}>
+                            <p>3. AI/Machine Learning Engineer</p>
+                        </div>
+                        <div className={styles.career + " " + styles.d}>
+                            <p>4. Cybersecurity Engineer</p>
+                        </div>
+                        <div className={styles.career + " " + styles.e}>
+                            <p>5. DevOps Engineer</p>
+                        </div>
+                    </div>
+                    <img src="/pie.png" alt="pie" />
+                </div>
+
+                <h4 className={styles.conTitle}>Your Progress</h4>
+                {!isStarted ? (
+                    <Empty
+                        image="https://gw.alipayobjects.com/zos/antfincdn/ZHrcdLPrvN/empty.svg"
+                        imageStyle={{ height: 60 }}
+                        description={
+                            <Typography.Text>
+                                You have not yet started any progress.
+                            </Typography.Text>
+                        }
+                    >
+                        <Button type="primary">Get Started</Button>
+                    </Empty>
+                ) : (
+                    progresses.map((progress, index) => {
+                        const currentStep = progress.completedStep - 1;
+
+                        return (
+                            <div key={index} className={styles.progress}>
+                                <h5>
+                                    {progress.type}: {progress.title}
+                                </h5>
+                                <Steps
+                                    direction="horizontal"
+                                    current={currentStep}
+                                    items={Array.from(
+                                        { length: progress.totalSteps },
+                                        (_, i) => ({
+                                            title: `${progress.subTitle} ${
+                                                i + 1
+                                            }`,
+                                            description:
+                                                i < progress.completedStep
+                                                    ? "Completed"
+                                                    : i === currentStep
+                                                    ? "In Progress"
+                                                    : "Pending",
+                                        })
+                                    )}
+                                />
+                            </div>
+                        );
+                    })
+                )}
+
+                <Button
+                    shape="circle"
+                    className={styles.btn}
+                    variant="outlined"
+                    icon={<WechatOutlined />}
+                    onClick={showLoading}
+                    size={"large"}
+                />
+                <Modal
+                    width={400}
+                    title={<p>LakshAI</p>}
+                    style={{
+                        position: "fixed",
+
+                        top: 10,
+                        right: 20,
+                    }}
+                    loading={loading}
+                    footer={null}
+                    open={open}
+                    onCancel={() => setOpen(false)}
+                >
+                    <Search
+                        placeholder="Ask me anything"
+                        allowClear
+                        enterButton="Send"
+                        size="large"
+                        onSearch={() => {}}
+                    />
+                </Modal>
+            </div>
+        </main>
+    );
 }
