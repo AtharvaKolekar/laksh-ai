be92d2fa6903269a7c1981d965ef620e7ac08775
Atharva Kolekar <118675133+AtharvaKolekar@users.noreply.github.com>
2024-11-08 18:23:48 +0530

    Added Mentor Page

--- src/app/(protected)/Dashboard/Mentors/page.jsx
+++ src/app/(protected)/Dashboard/Mentors/page.jsx
@@ -1,37 +1,44 @@
 "use client";
 import { useState, useEffect } from "react";
 import styles from "./page.module.css";
-import Header from "@/components/chat/header/Header";
 
-import { signOut, useUser } from "@/lib/auth";
+import { useUser } from "@/lib/auth";
 import { initFirebase } from "@/lib/firebase";
 
 import { getDatabase, ref, set, get, child } from "firebase/database";
 import { useRouter } from "next/navigation";
 
 import Cal, { getCalApi } from "@calcom/embed-react";
-
+import { Button, Modal, Empty, Skeleton, Typography } from "antd";
+import { ExportOutlined } from '@ant-design/icons';
 
 export default function Dashboard() {
     const router = useRouter();
     const app = initFirebase();
     const database = getDatabase(app);
     const user = useUser();
+    const [showCal, setShowCal] = useState(false);
+    const [isBooked, setIsBooked] = useState(false);
+    const [bookingData, setBookingData] = useState({});
+    const [date, setDate] = useState("");
+    const [duration, setDuration] = useState("");
+    const [meetingUrl, setMeetingUrl] = useState("");
 
-    const [name, setName] = useState("");
-
-    if (user) {
-        console.log(user);
-        get(child(ref(database), `UserData/${user.uid}/details`)).then(
-            (snapshot) => {
-                if (snapshot.exists()) {
-                    setName(snapshot.val().name);
-                } else {
-                    console.log("No data available");
-                }
-            }
-        );
-    }
+    useEffect(() => {
+        if (isBooked) {
+            const data = bookingData;
+            const startDate = new Date(data.startTime), endDate = new Date(data.endTime);
+            const date = startDate.toLocaleDateString("en-US", { weekday: "long", year: "numeric", month: "long", day: "numeric", timeZone: data.timeZone });
+            const startTimeFormatted = startDate.toLocaleTimeString("en-US", { hour: "numeric", minute: "numeric", hour12: true, timeZone: data.timeZone });
+            const endTimeFormatted = endDate.toLocaleTimeString("en-US", { hour: "numeric", minute: "numeric", hour12: true, timeZone: data.timeZone });
+            const timeZoneName = startDate.toLocaleTimeString("en-US", { timeZone: data.timeZone, timeZoneName: "long" }).split(' ').slice(-3).join(' ');
+            const duration = `${startTimeFormatted} - ${endTimeFormatted} (${timeZoneName})`;
+        
+            setDate(date);
+            setDuration(duration);
+            setMeetingUrl(data.meetingUrl);
+        }
+    }, [isBooked, bookingData]);
 
     useEffect(() => {
         (async function () {
@@ -40,9 +47,22 @@ export default function Dashboard() {
             });
             cal("ui", {
                 theme: "light",
-                styles: { branding: { brandColor: "#6100FF" } },
+                styles: { branding: { brandColor: "#1677ff" } },
                 hideEventTypeDetails: false,
                 layout: "month_view",
+            });
+            cal("on", {
+                action: "bookingSuccessful",
+                callback: (e) => {
+                    setIsBooked(true);
+                    const bookedData = {
+                        startTime : e.detail.data.booking.startTime,
+                        endTime : e.detail.data.booking.endTime,
+                        timeZone : e.detail.data.eventType.schedule.timeZone,
+                        meetingUrl : e.detail.data.booking.references[0].meetingUrl
+                    }
+                    setBookingData(bookedData);
+                },
             });
         })();
     }, []);
@@ -50,17 +70,63 @@ export default function Dashboard() {
     return (
         <main>
             <div className={styles.container}>
-                <Cal
-                    namespace="connect-with-expert-mentor"
-                    calLink="lashai-testing/connect-with-expert-mentor"
-                    style={{
-                        width: "100%",
-                        height: "100%",
-                        overflow: "scroll",
-                    }}
-                    config={{ layout: "month_view", theme: "light" }}
-                />
+                <MainScreen isVisible={showCal} callback={() => setShowCal(true)} isBooked={isBooked} bookedData={bookingData} date={date} duration={duration} meetingUrl={meetingUrl}/>
+                <CalScreen isVisible={showCal} callback={() => setShowCal(false)} isBooked={isBooked} />
             </div>
         </main>
+    );
+}
+
+function MainScreen({ isVisible, callback, isBooked, bookedData, date, duration, meetingUrl }) {
+    return (
+      <div style={{ display: isVisible ? "none" : "block" }}>
+        {isBooked && bookedData ? (
+            <div className={styles.booking}>
+                <img src="/meet.webp" alt="meet" />
+                <div className={styles.bookingContent}>    
+                    <h3>Meeting Scheduled</h3>
+                    <p>{date}Thursday, November 21, 2024</p>
+                    <p>{duration}7:30 PM - 9:30 PM (India Standard Time)</p>
+                    <Button type="primary" size="middle" onClick={() => window.open(meetingUrl, "_blank")}><strong>Join Meeting <ExportOutlined /></strong></Button>
+                </div>
+            </div>
+        ): (
+            <Empty
+                image="https://gw.alipayobjects.com/zos/antfincdn/ZHrcdLPrvN/empty.svg"
+                imageStyle={{ height: 60 }}
+                description={
+                    <Typography.Text>
+                        No meetings scheduled
+                    </Typography.Text>
+                }
+            >
+                <Button type="primary" onClick={callback}>Schedule Now</Button>
+            </Empty>
+        )}
+      </div>
+    )
+}
+
+function CalScreen({ isVisible, callback, isBooked }) {
+    return (
+        <div style={{ display: isVisible ? "block" : "none" }}>
+            <Button style={{ marginBottom: 15, display: isBooked ? "none" : "block" }} type="primary" onClick={callback}><strong>Back</strong></Button>
+            <Cal
+                namespace="connect-with-expert-mentor"
+                calLink="lashai-testing/connect-with-expert-mentor"
+                style={{
+                    width: "100%",
+                    height: "100%",
+                    overflow: "scroll",
+                    display: isVisible ? "block" : "none",
+                }}
+                config={{ layout: "month_view", theme: "light" }}
+            />
+            <div style={{ width: "100%", display: isBooked ? "flex" : "none", justifyContent: 'center' }}> 
+              <Button style={{ width: "100%", maxWidth: 576}} type="primary" onClick={callback}><strong>Continue</strong></Button>
+            </div>
+
+
+        </div>
     );
 }
